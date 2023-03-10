fa438edcc071c9c3f2cdb120ca3b4fc888bd9086
ChristianAmes <christian-amesoeder@web.de>
2023-03-10 09:58:06 +0100

    Adapted to also get sum of top three

--- code01/src/main.rs
+++ code01/src/main.rs
@@ -17,6 +17,8 @@ fn main() {
 
     let mut max:u128 = 0;
     let mut moving_sum:u128 = 0;
+    let mut top_three:[u128;3] = [0,0,0];  
+
     for line in contents.lines(){
         
         if line != "" {
@@ -27,14 +29,24 @@ fn main() {
             if moving_sum > max {
                 max = moving_sum;
             }
+
+            if top_three[0] < moving_sum{
+                
+                top_three[0] = moving_sum; 
+                top_three.sort();
+            }    
+                
             moving_sum = 0;
         }
 
     }
 
-
+    
     println!("Maximum : {}", max);
 
+    let sum_top_three:u128 = top_three.iter().sum();
+
+    println!("SUm of top 3 :{}", sum_top_three);
 
 }
 
